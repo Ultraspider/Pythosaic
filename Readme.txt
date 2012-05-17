7702954ed3c593187e73489c9660d5ff847c8195
Sebastien Lemmens <ultraspider@gmail.com>
2012-05-17 13:02:56 +0200

    Can now load any file format supported by PIL. Quantization by PIL is not as good as with IM convert

--- Readme.txt
+++ Readme.txt
@@ -2,6 +2,26 @@ Python program to transform a picture into a mosaic of smaller pictures.
 
 Work in progress
 
-Input is a raw image prepared with ImageMagick's convert:
+Usage: mosaic.py [options] FILE
 
-e.g: convert -resize 20% picture.jpg -depth 8 -colors 32 rgb:rpicture
+Options:
+  -h, --help            show this help message and exit
+  -p PIXEL_SIZE, --psize=PIXEL_SIZE
+                        Size of the small images.
+  -o OUTPUT, --output=OUTPUT
+                        Output file.
+  -d, --dither          Use dithering instead of adaptive palette
+  -r RAW_WIDTH, --raw_width=RAW_WIDTH
+                        width of raw file if input file is a raw RGB image
+  -c COLCOUNT, --col_count=COLCOUNT
+                        Color count for color reduction, Default: 32
+
+if using -r, file must be a raw RGB (3 bytes per pixel) image prepared with another software like ImageMagick's convert:
+convert -resize 20% picture.jpg -depth 8 -colors 32 rgb:rpicture
+adapt the above to your liking, see convert manual for info about parameters
+Note: -d and -c are ineffective when -r is used as the raw image is used without further processing.
+
+RAW_WIDTH is the width of the raw file = (original image width)*(resize parameters of convert)
+
+Samples (pictures that will compose the mosaic) have to be put in <working directory>/samples/
+
--- mosaic.py
+++ mosaic.py
@@ -3,24 +3,71 @@
 import os
 from optparse import OptionParser
 from PIL import Image
+import scipy
+import scipy.misc
+import scipy.cluster
 
 def load_samples(pixel_size):
    print("loading samples:"),
    samples = []
+   NUM_CLUSTERS = 3
    path = 'samples'
+
    for infile in os.listdir(path):
-      im = Image.open(os.path.join(path,infile))
-      res = im.resize([1,1], Image.ANTIALIAS)
-      twenty = im.resize([pixel_size,pixel_size], Image.ANTIALIAS)
-      if res.mode is 'L':
-         res = res.convert('RGB')
+      sample = Image.open(os.path.join(path,infile))
+      if sample.mode is not 'RGB':
+         sample = sample.convert('RGB')
+      
+      resized_sample = sample.resize([pixel_size,pixel_size], Image.ANTIALIAS)
+      
+      res = sample.resize([1,1], Image.ANTIALIAS) #used to approximate the dominant color of the image
       col = [ord(x) for x in res.tostring()]
-      samples.append([twenty, col])
+
+#     ar = scipy.misc.fromimage(sample.resize([100,100]))
+#     shape = ar.shape
+#     ar = ar.reshape(scipy.product(shape[:2]), shape[2])
+
+#     codes, dist = scipy.cluster.vq.kmeans(ar, NUM_CLUSTERS)
+
+#     vecs, dist = scipy.cluster.vq.vq(ar, codes)         # assign codes
+#     counts, bins = scipy.histogram(vecs, len(codes))    # count occurrences
+
+#     index_max = scipy.argmax(counts)                    # find most frequent
+#     peak = codes[index_max]
+
+#      col = peak
+
+      samples.append([resized_sample, col])
       print('.'),
    print("Done")
    return samples
 
-def load_image(filename):
+def load_image(filename, colcount, dither):
+   print("reading image..."),
+   image = []
+   pixels = {}
+   im = Image.open(filename)
+   size = im.size
+   im = im.resize([size[0]/5, size[1]/5], Image.ANTIALIAS)
+   
+   if im.mode is not 'RGB':
+      im = im.convert('RGB')
+   if dither:
+      im = im.convert("P", colors=colcount)
+   else:
+      im = im.convert("P", palette=Image.ADAPTIVE, colors=colcount)
+   im = im.convert('RGB')
+   
+   im_raw = im.tostring()
+   for i in range(0, len(im_raw), 3):
+      pixel = im_raw[i]+im_raw[i+1]+im_raw[i+2]
+      if pixel not in pixels.keys():
+         pixels[pixel] = [[ord(x) for x in pixel], None]
+      image.append(pixel)
+   print("Done")
+   return image, pixels, im.size
+
+def load_raw(filename, raw_width):
    print("reading image..."),
    image = []
    pixels = {}
@@ -34,8 +81,9 @@ def load_image(filename):
          pixel = f.read(3)
    finally:
       f.close()
+   size = [raw_width,len(image)/raw_width]
    print("Done")
-   return image, pixels
+   return image, pixels, size
 
 def matchcolor(samples, pixels):
    pix = dict(pixels)
@@ -57,36 +105,45 @@ def matchcolor(samples, pixels):
 
 
 def main():
-   usage = "usage: %prog [options] FILE WIDTH"
-   parser = OptionParser()
+   usage = "usage: %prog [options] FILE"
+   parser = OptionParser(usage=usage)
    parser.add_option("-p", "--psize", type='int', dest="pixel_size", help="Size of the small images.", default=20)
    parser.add_option("-o", "--output", type='string', dest="output", help="Output file.", default="out.jpg")
+   parser.add_option("-d", "--dither", action='store_true', dest="dither", help="Use dithering instead of adaptive palette", default=False)
+   parser.add_option("-r", "--raw_width", type='int', dest="raw_width", help="width of raw file if input file is a raw RGB image", default=None)
+   parser.add_option("-c", "--col_count", type='int', dest="colcount", help="Color count for color reduction, Default: 32", default=32)
 
    (options, args) = parser.parse_args()
 
    filename = args[0]
-   image_size = int(args[1])
    pixel_size = options.pixel_size
    outfile = options.output
+   dither = options.dither
+   colcount = options.colcount
+   raw_width = options.raw_width
 
    dimension = {'x': None, 'y': None}
 
-   image, pixels = load_image(filename)
+   if raw_width:
+      image, pixels, size = load_raw(filename, raw_width)
+   else:
+      image, pixels, size = load_image(filename, colcount, dither)
 
-   dimension['x'] = image_size*pixel_size
-   dimension['y'] = (len(image)/image_size)*pixel_size
+   dimension['x'] = size[0]
+   dimension['y'] = size[1]
 
-   buffer = Image.new('RGB', (dimension['x'],dimension['y']))
+   buffer = Image.new('RGB', (dimension['x'] * pixel_size, dimension['y']*pixel_size))
 
    samples = load_samples(pixel_size)
 
    matchcolor(samples, pixels)
 
+   print("assembling and saving output image: %s [%s x %s]" % (outfile, dimension['x']*pixel_size, 
+                                                                        dimension['y']*pixel_size))
 
-   print("assembling and saving output image: %s [%s x %s]" % (outfile, dimension['x'], dimension['y']))
-   for i in range(image_size):
-      for j in range(len(image)/image_size):
-         image_idx = image[i+image_size*j]
+   for i in range(dimension['x']):
+      for j in range(dimension['y']):
+         image_idx = image[i+dimension['x']*j]
          buffer.paste(pixels[image_idx][1],(i*pixel_size, j*pixel_size))
 
    buffer.save(outfile, "JPEG")
